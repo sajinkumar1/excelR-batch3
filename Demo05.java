8cc372fd59d4dfe006db3e367e2da1a9b9a3d49a
sajinkumar1 <sajinkumar441@gmail.com>
2025-01-03 11:12:51 +0530

    creadtial check

---
+++ Demo05.java
@@ -0,0 +1,10 @@
+public class Demo05{
+    punlic static void main(String[] args){
+        String username = "user01";
+        String pwd = "user_01";
+        While(username == "user01") && (pwd == "user_01"){
+            System.out.println("Hello happy new year");
+            break;
+        }
+    }
+}
