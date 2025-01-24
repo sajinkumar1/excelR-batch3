b56d213c26527fc33ecf7691e597d9e2ec3ed5d2
sajinkumar1 <sajinkumar441@gmail.com>
2025-01-24 12:53:29 +0530

    Palindrome code from Leetcode

---
+++ Ltcode001.java
@@ -0,0 +1,27 @@
+class Ltcode001 {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+
+    public static void main(String[] args) {
+        
+        Ltcode001 solution = new Ltcode001();
+
+        
+        System.out.println(solution.isPalindrome(121)); 
+        System.out.println(solution.isPalindrome(-121)); 
+        System.out.println(solution.isPalindrome(10));   
+        System.out.println(solution.isPalindrome(0));    
+    }
+}
