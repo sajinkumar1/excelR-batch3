e9dbc5751f528cae4c1332e49ae9765d78c6cd69
sajinkumar1 <sajinkumar441@gmail.com>
2025-01-27 12:55:21 +0530

    to print 5 table

---
+++ Demo050.java
@@ -0,0 +1,8 @@
+
+class Demo050 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("5 * " + i + " = " + 5 * i);
+        }
+    }
+}
